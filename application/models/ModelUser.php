09b2492f6263b6d416bf2eae1c141491f516d080
Aimmzz <rohimkurniawan456@gmail.com>
2021-10-25 17:02:37 +0700

    membuat modelbuku.php , model modeluser.php dan menambahkan database

---
+++ application/models/ModelUser.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('no direct script access allowed');
+
+class ModelUser extends CI_Model{
+
+    public function simpanData($data = null){
+        return $this->db-get_where('user' , $where);
+    }
+
+    public function getUserWhere($where = null){
+        return $this->db->get_where('user' , $where);
+    }
+
+    public function cekUserAccess($where = null){
+        $this->db->select('*');
+        $this->db->form('access_menu');
+        $this->db->where('$where');
+        return $this->db->get();
+    }
+    public function getUserLimit(){
+        $this->db->select('*');
+        $this->db->form('user');
+        $this->db->limit(10 , 0);
+        return $this->db->get();
+    }
+}
